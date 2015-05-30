1029a2886e758534df55f93132b45bce422f434d
MichaelBuen <i.enable.much@gmail.com>
2015-05-30 22:20:20 +0800

    convert the solution to LightInject

--- SpaArchitectureMvp/Global.asax.cs
+++ SpaArchitectureMvp/Global.asax.cs
@@ -2,7 +2,8 @@
 using System.Linq;
 using System.Web.Http;
 
-using DryIoc;
+using System.Net.Http; // .RegisterForDispose extension method
+
 using System.Reflection;
 
 using UnitTestFriendlyDal;
@@ -11,29 +12,33 @@ namespace SpaArchitectureMvp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        DryIoc.Container _container;
+        LightInject.IServiceContainer _container;
+        
 
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
 
-            _container = new DryIoc.Container();
+            _container = new LightInject.ServiceContainer();
                         
 
             Assembly assembly = typeof(WebApiApplication).Assembly;
 
             foreach (var controller in assembly.GetTypes().Where(t => typeof(ApiController).IsAssignableFrom(t)))
             {
-                _container.Register(controller, DryIoc.Reuse.InResolutionScope);                
+                _container.Register(controller, new LightInject.PerScopeLifetime()); 
             }
 
-            _container.Register<ISampleService, SampleService>(DryIoc.Reuse.Singleton);
+            // LightInject's singleton is PerContainerLifetime
+            // http://stackoverflow.com/questions/26948063/resolve-instances-in-static-functions-using-lightinject
+            _container.Register<ISampleService, SampleService>(new LightInject.PerContainerLifetime());
 
-            _container.RegisterDelegate<NHibernate.ISessionFactory>(x => DomainMapping.Mapper.SessionFactory, DryIoc.Reuse.Singleton);
+            _container.Register<NHibernate.ISessionFactory>(factory => {
+                return DomainMapping.Mapper.SessionFactory;
+            }, new LightInject.PerContainerLifetime());
 
-            _container.RegisterDelegate<IDomainAccessFactory>(x => new DomainAccessFactory(_container.Resolve<NHibernate.ISessionFactory>()), DryIoc.Reuse.Singleton);
-
+            _container.Register<IDomainAccessFactory>(factory => new DomainAccessFactory(_container.GetInstance<NHibernate.ISessionFactory>()), new LightInject.PerContainerLifetime());
 
             GlobalConfiguration.Configuration.Services.Replace(typeof(System.Web.Http.Dispatcher.IHttpControllerActivator), new ControllerActivator(_container));
 
@@ -52,9 +57,9 @@ namespace SpaArchitectureMvp
     // http://blog.ploeh.dk/2012/10/03/DependencyInjectioninASP.NETWebAPIwithCastleWindsor/
     public class ControllerActivator : System.Web.Http.Dispatcher.IHttpControllerActivator
     {
-        DryIoc.Container _container;
+        LightInject.IServiceContainer _container;
 
-        public ControllerActivator(DryIoc.Container container)
+        public ControllerActivator(LightInject.IServiceContainer container)
         {
             _container = container;
         }
@@ -65,12 +70,16 @@ namespace SpaArchitectureMvp
             Type controllerType)
         {
 
-            var controller = (System.Web.Http.Controllers.IHttpController)_container.Resolve(controllerType);
+            using (_container.BeginScope())
+            {
+                var controller = (System.Web.Http.Controllers.IHttpController)_container.GetInstance(controllerType);
+                return controller;
+            }
+            
+            
+        }
 
-            _container.ResolvePropertiesAndFields(controller);
-
-            return controller;
-        }       
+      
     }
 
     public interface ISampleService
